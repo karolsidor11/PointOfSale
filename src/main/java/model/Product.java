79a80ca8eaee06da93598e093dcb272a98e4ee32
KursJava <kursjava1@wp.pl>
2019-02-22 15:50:03 +0100

    init commit

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,63 @@
+package model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Product {
+
+    private String name;
+    private BigDecimal price;
+    private String barcode;
+
+    public Product() {
+    }
+
+    public Product(String name, BigDecimal price, String barcode) {
+        this.name = name;
+        this.price = price;
+        this.barcode = barcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name) && Objects.equals(price, product.price) && Objects.equals(barcode, product.barcode);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, price, barcode);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "name='" + name + '\'' + ", price=" + price + ", barcode='" + barcode + '\'' + '}';
+    }
+}
